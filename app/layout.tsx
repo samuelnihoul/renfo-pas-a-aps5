1e90db85938e51dde216a3317d73f64d4ca9ac1c
samuelnihoul <samuel.nihoul.ollivard@protonmail.ch>
2025-05-21 21:14:01 +0200

    nihoul.dev

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import "./globals.css"
+import "@/styles/globals.css"
 import { DataProvider } from "@/components/data-provider"
 import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "MuscuFit - Application de renforcement musculaire",
+  title: "Renfo Pas à Pas - Application de renforcement musculaire",
   description: "Application de programme de renforcement musculaire avec médiathèque d'exercices",
-    generator: 'v0.dev'
+  generator: 'nihoul.dev'
 }
 
 export default function RootLayout({
