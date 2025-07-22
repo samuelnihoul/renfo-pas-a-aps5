295d212a9fe5ff5e5d4b644d081215ed4df3e32b
samuelnihoul <69475809+samuelnihoul@users.noreply.github.com>
2025-07-22 18:52:21 +0200

    rm the spcase in the migrations

--- rfppp.session.sql
+++ rfppp.session.sql
@@ -1,44 +1,44 @@
 CREATE TABLE IF NOT EXISTS "accounts" (
-	"id" varchar(255) PRIMARY KEY NOT NULL,
-	"userId" varchar(255) NOT NULL,
-	"type" varchar(255) NOT NULL,
-	"provider" varchar(255) NOT NULL,
-	"providerAccountId" varchar(255) NOT NULL,
-	"refresh_token" varchar(255),
-	"access_token" varchar(255),
-	"expires_at" integer,
-	"token_type" varchar(255),
-	"scope" varchar(255),
-	"id_token" varchar(255),
-	"session_state" varchar(255)
+    "id" varchar(255) PRIMARY KEY NOT NULL,
+    "userId" varchar(255) NOT NULL,
+    "type" varchar(255) NOT NULL,
+    "provider" varchar(255) NOT NULL,
+    "providerAccountId" varchar(255) NOT NULL,
+    "refresh_token" varchar(255),
+    "access_token" varchar(255),
+    "expires_at" integer,
+    "token_type" varchar(255),
+    "scope" varchar(255),
+    "id_token" varchar(255),
+    "session_state" varchar(255)
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "sessions" (
-	"id" varchar(255) PRIMARY KEY NOT NULL,
-	"sessionToken" varchar(255) NOT NULL,
-	"userId" varchar(255) NOT NULL,
-	"expires" timestamp NOT NULL,
-	CONSTRAINT "sessions_sessionToken_unique" UNIQUE("sessionToken")
+    "id" varchar(255) PRIMARY KEY NOT NULL,
+    "sessionToken" varchar(255) NOT NULL,
+    "userId" varchar(255) NOT NULL,
+    "expires" timestamp NOT NULL,
+    CONSTRAINT "sessions_sessionToken_unique" UNIQUE("sessionToken")
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "user_programs" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"user_id" varchar(255) NOT NULL,
-	"program_id" integer NOT NULL,
-	"purchase_date" timestamp with time zone DEFAULT now(),
-	"created_at" timestamp with time zone DEFAULT now(),
-	"updated_at" timestamp with time zone DEFAULT now()
+    "id" serial PRIMARY KEY NOT NULL,
+    "user_id" varchar(255) NOT NULL,
+    "program_id" integer NOT NULL,
+    "purchase_date" timestamp with time zone DEFAULT now(),
+    "created_at" timestamp with time zone DEFAULT now(),
+    "updated_at" timestamp with time zone DEFAULT now()
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "verificationToken" (
-	"identifier" varchar(255) NOT NULL,
-	"token" varchar(255) NOT NULL,
-	"expires" timestamp NOT NULL,
-	CONSTRAINT "verificationToken_identifier_token_unique" UNIQUE("identifier", "token")
+    "identifier" varchar(255) NOT NULL,
+    "token" varchar(255) NOT NULL,
+    "expires" timestamp NOT NULL,
+    CONSTRAINT "verificationToken_identifier_token_unique" UNIQUE("identifier", "token")
 );
 --> statement-breakpoint
 ALTER TABLE "blocks"
-	RENAME COLUMN "sets" TO "instructions";
+    RENAME COLUMN "sets" TO "instructions";
 --> statement-breakpoint
 ALTER TABLE "blocks" DROP CONSTRAINT "day_exercise_unique_idx";
 --> statement-breakpoint
